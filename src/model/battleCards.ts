1d850e8a7a02e019b96dc7a626e23516eba9abfd
delphin92 <delphin92@list.ru>
2022-07-12 01:25:54 +0300

    Add all troop types and some cards

--- src/model/battleCards.ts
+++ src/model/battleCards.ts
@@ -1,5 +1,96 @@
+import {TroopTypes} from "model/troops";
+
 export interface BattleCard {
   name: string;
-  cost: number;
+  cost: number | CostObject;
+  reference: number;
   rule: string;
+  types: (keyof typeof BattleCardType)[];
 }
+
+export interface CostObject {
+  default: number;
+  special: Partial<Record<TroopTypes, number>>;
+}
+
+export enum BattleCardType {
+  Army = 'Army',
+  Transfer = 'Transfer',
+  Hero = 'Hero',
+  Event = 'Event',
+  Stand = 'Stand',
+}
+
+export const battleCards: BattleCard[] = [{
+  name: "Ambush: home only",
+  cost: 1,
+  reference: 24,
+  rule: '',
+  types: [
+    "Army"
+  ]
+}, {
+  name: "Ambush: any topo",
+  cost: 2,
+  reference: 24,
+  rule: '',
+  types: [
+    "Army"
+  ]
+}, {
+  name: "Armored",
+  cost: 0.5,
+  reference: 25,
+  rule: '',
+  types: [
+    "Stand",
+    "Transfer"
+  ]
+}, {
+  name: "Assassin",
+  cost: 1,
+  reference: 26,
+  rule: '',
+  types: [
+    "Hero"
+  ]
+}, {
+  name: "Away",
+  cost: 0.5,
+  reference: 27,
+  rule: '',
+  types: [
+    "Hero",
+    "Stand"
+  ]
+}, {
+  name: "Away (Transfer)",
+  cost: 1,
+  reference: 27,
+  rule: '',
+  types: [
+    "Transfer"
+  ]
+}, {
+  name: "Brittle",
+  cost: {
+    default: -1,
+    special: {
+      'Artillery': 0,
+      'Pavisiers': -2
+    }
+  },
+  reference: 28,
+  rule: '',
+  types: [
+    "Stand"
+  ]
+}, {
+  name: "Charge Through",
+  cost: 1,
+  reference: 29,
+  rule: '',
+  types: [
+    "Event"
+  ]
+}];
--- src/model/troops.ts
+++ src/model/troops.ts
@@ -1,12 +1,12 @@
 export interface TroopType {
-  name: string;
+  name: TroopTypes;
   type: 'foot' | 'mounted';
   order: 'open' | 'close';
   cost: number;
-  modifiers: {
-    vsFoot: number;
-    vsMounted: number;
-  }
+  vsFoot: number;
+  vsMounted: number;
+  shoot?: number;
+  target: number
   move: number;
   shatter?: TroopType[];
   shattered?: TroopType[];
@@ -15,22 +15,239 @@ export interface TroopType {
   additional?: string[];
 }
 
-export const openOrderFootTypes: TroopType[] = [{
-  name: 'Rabble',
-  type: 'foot',
-  order: 'open',
-  cost: 2,
-  modifiers: {
-    vsFoot: 2,
-    vsMounted: 1
-  },
-  move: 3,
-  shattered: []   // TODO
-}];
+export const troopTypeNames = [
+  'Archers',
+  'Bow Levy',
+  'Light Foot',
+  'Light Spear',
+  'Rabble',
+  'Raiders',
+  'Skirmishers',
+  'Warband',
 
-export const closeOrderFootTypes: TroopType[] = [];
-export const openOrderMountedTypes: TroopType[] = [];
-export const closeOrderMountedTypes: TroopType[] = [];
+  'Artillery',
+  'Elite Foot',
+  'Heavy Foot',
+  'Horde',
+  'Pavisiers',
+  'Pike',
+  'Spear',
+  'War Wagons',
+  'Warriors',
+
+  'Bad Horse',
+  'Battle Taxi',
+  'Chariots',
+  'Elite Cavalry',
+  'Horse Bow',
+  'Javelin Cavalry',
+  'Knights',
+
+  'Cataphracts',
+  'Elephants',
+] as const;
+
+export type TroopTypes = typeof troopTypeNames[number];
+
+const convert = (type: 'foot' | 'mounted', order: 'open' | 'close') => (data: Partial<TroopType>[]) => data.map(toopType => ({
+  ...toopType,
+  type,
+  order,
+} as TroopType));
+
+export const openOrderFootTypes: TroopType[] = convert('foot', 'open')([{
+  name: "Archers",
+  vsFoot: 2,
+  vsMounted: 4,
+  shoot: 3,
+  target: 3,
+  move: 3,
+  cost: 4
+}, {
+  name: "Bow Levy",
+  vsFoot: 2,
+  vsMounted: 3,
+  target: 3,
+  move: 3,
+  cost: 2
+}, {
+  name: "Light Foot",
+  vsFoot: 3,
+  vsMounted: 2,
+  target: 3,
+  move: 5,
+  cost: 3
+}, {
+  name: "Light Spear",
+  vsFoot: 3,
+  vsMounted: 3,
+  target: 3,
+  move: 4,
+  cost: 3
+}, {
+  name: "Rabble",
+  vsFoot: 2,
+  vsMounted: 1,
+  target: 2,
+  move: 4,
+  cost: 2
+}, {
+  name: "Raiders",
+  vsFoot: 4,
+  vsMounted: 2,
+  target: 3,
+  move: 4,
+  cost: 4
+}, {
+  name: "Skirmishers",
+  vsFoot: 2,
+  vsMounted: 1,
+  target: 3,
+  move: 5,
+  cost: 3
+}, {
+  name: "Warband",
+  vsFoot: 3,
+  vsMounted: 2,
+  target: 3,
+  move: 4,
+  cost: 3
+}]);
+
+export const closeOrderFootTypes: TroopType[] = convert('foot', 'open')([{
+  name: "Artillery",
+  vsFoot: 2,
+  vsMounted: 2,
+  shoot: 4,
+  target: 3,
+  move: 2,
+  cost: 3
+}, {
+  name: "Elite Foot",
+  vsFoot: 5,
+  vsMounted: 3,
+  target: 3,
+  move: 3,
+  cost: 4
+}, {
+  name: "Heavy Foot",
+  vsFoot: 4,
+  vsMounted: 3,
+  target: 3,
+  move: 3,
+  cost: 3
+}, {
+  name: "Horde",
+  vsFoot: 3,
+  vsMounted: 2,
+  target: 2,
+  move: 2,
+  cost: 2
+}, {
+  name: "Pavisiers",
+  vsFoot: 3,
+  vsMounted: 4,
+  shoot: 3,
+  target: 3,
+  move: 3,
+  cost: 4
+}, {
+  name: "Pike",
+  vsFoot: 3,
+  vsMounted: 4,
+  target: 3,
+  move: 3,
+  cost: 3
+}, {
+  name: "Spear",
+  vsFoot: 4,
+  vsMounted: 4,
+  target: 3,
+  move: 3,
+  cost: 4
+}, {
+  name: "War Wagons",
+  vsFoot: 3,
+  vsMounted: 4,
+  shoot: 3,
+  target: 4,
+  move: 2,
+  cost: 3
+}, {
+  name: "Warriors",
+  vsFoot: 3,
+  vsMounted: 2,
+  target: 3,
+  move: 3,
+  cost: 3
+}]);
+
+export const openOrderMountedTypes: TroopType[] = convert('foot', 'open')([{
+  name: "Bad Horse",
+  vsFoot: 2,
+  vsMounted: 2,
+  target: 2,
+  move: 6,
+  cost: 3
+}, {
+  name: "Battle Taxi",
+  vsFoot: 2,
+  vsMounted: 2,
+  target: 2,
+  move: 6,
+  cost: 3
+}, {
+  name: "Chariots",
+  vsFoot: 2,
+  vsMounted: 3,
+  target: 3,
+  move: 6,
+  cost: 4
+}, {
+  name: "Elite Cavalry",
+  vsFoot: 3,
+  vsMounted: 3,
+  target: 3,
+  move: 6,
+  cost: 4
+}, {
+  name: "Horse Bow",
+  vsFoot: 2,
+  vsMounted: 3,
+  target: 2,
+  move: 8,
+  cost: 4
+}, {
+  name: "Javelin Cavalry",
+  vsFoot: 3,
+  vsMounted: 2,
+  target: 2,
+  move: 8,
+  cost: 4
+}, {
+  name: "Knights",
+  vsFoot: 3,
+  vsMounted: 4,
+  target: 2,
+  move: 5,
+  cost: 4
+}]);
+
+export const closeOrderMountedTypes: TroopType[] = convert('foot', 'open')([{
+  name: "Cataphracts",
+  vsFoot: 4,
+  vsMounted: 4,
+  target: 3,
+  move: 4,
+  cost: 4
+}, {
+  name: "Elephants",
+  vsFoot: 5,
+  vsMounted: 4,
+  target: 3,
+  move: 4,
+  cost: 4
+}]);
 
 export const troopTypes = [
   ...openOrderFootTypes,
