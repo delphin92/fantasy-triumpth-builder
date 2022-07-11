29a040f6e8acf2f02c53f9e90f402cf964b51638
delphin92 <delphin92@list.ru>
2022-07-12 02:43:03 +0300

    Header layout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Nav, Navbar } from "react-bootstrap";
+import {Container, Nav, Navbar} from "react-bootstrap";
 import {Link, usePath} from "raviger";
 
 interface HeaderProps {
@@ -8,15 +8,17 @@ interface HeaderProps {
 
 const Header: React.FC<HeaderProps> = () => (
   <Navbar bg="light" expand="lg" className="mb-2">
-    <Navbar.Brand as={Link} href="/">Fantasy Triumph Army Builder</Navbar.Brand>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="mr-auto" activeKey={usePath() ?? ''}>
-        <Nav.Link as={Link} href="/">Army Template</Nav.Link>
-        <Nav.Link as={Link} href="/list">Army List</Nav.Link>
-        <Nav.Link as={Link} href="/game">Game Mode</Nav.Link>
-      </Nav>
-    </Navbar.Collapse>
+    <Container>
+      <Navbar.Brand as={Link} href="/">Fantasy Triumph Army Builder</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto" activeKey={usePath() ?? ''}>
+          <Nav.Link as={Link} href="/">Army Template</Nav.Link>
+          <Nav.Link as={Link} href="/list">Army List</Nav.Link>
+          <Nav.Link as={Link} href="/game">Game Mode</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
   </Navbar>
 );
 
